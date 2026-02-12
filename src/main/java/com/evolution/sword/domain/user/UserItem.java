9540787f26f6a7cc9907c88bace5f0bfc11e6e7d
hyeonju <hyeonju.dlf@gmail.com>
2026-02-13 00:45:08 +0900

    feat :: UserItem builder 패턴 적용
    - baseItem 추가 함수

--- src/main/java/com/evolution/sword/domain/user/UserItem.java
+++ src/main/java/com/evolution/sword/domain/user/UserItem.java
@@ -1,6 +1,7 @@
 package com.evolution.sword.domain.user;
 
 import com.evolution.sword.domain.ItemMetadata;
+import com.evolution.sword.domain.ItemType;
 import jakarta.persistence.*;
 import org.hibernate.annotations.SoftDelete;
 import org.hibernate.annotations.SoftDeleteType;
@@ -22,6 +23,70 @@ public class UserItem {
     @JoinColumn(name = "metadata_id")
     private ItemMetadata itemMetadata;
 
+    private ItemType itemType;
     private Integer currentLevel;
+
+    protected UserItem() {}
+
+    public UserItem(Builder builder){
+        this.user = builder.user;
+        this.itemMetadata = builder.itemMetadata;
+        this.itemType = builder.itemType;
+        this.currentLevel = builder.currentLevel;
+    }
+
+    public ItemType getItemType() {
+        return itemType;
+    }
+
+    public Integer getCurrentLevel() {
+        return currentLevel;
+    }
+
+    public void upgrade(ItemMetadata itemMetadata){
+        this.itemMetadata = itemMetadata;
+        this.currentLevel = itemMetadata.getEnhancementLevel();
+    }
+
+    public Builder builder(){
+        return new Builder();
+    }
+
+    public static class Builder{
+        private User user;
+        private ItemMetadata itemMetadata;
+        private ItemType itemType;
+        private Integer currentLevel;
+
+        public Builder user(User user){
+            this.user = user;
+            return this;
+        }
+        public Builder itemMetadata(ItemMetadata itemMetadata){
+            this.itemMetadata = itemMetadata;
+            return this;
+        }
+        public Builder itemType(ItemType itemType){
+            this.itemType = itemType;
+            return this;
+        }
+        public Builder currentLevel(Integer currentLevel){
+            this.currentLevel = currentLevel;
+            return this;
+        }
+
+        public UserItem build(){
+            return new UserItem(this);
+        }
+    }
+
+    public static UserItem createBaseItem(User user, ItemMetadata metadata){
+        return new Builder()
+                .user(user)
+                .itemMetadata(metadata)
+                .itemType(metadata.getItemType())
+                .currentLevel(0)
+                .build();
+    }
 
 }
